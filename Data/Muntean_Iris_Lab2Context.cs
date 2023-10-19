39abe3e27f742e21b1b14801a0b7cfeeaec448bf
IRIS-GENȚIANA MUNTEAN <iris.muntean@stud.ubbcluj.ro>
2023-10-19 17:50:58 +0300

    Add project files.

---
+++ Data/Muntean_Iris_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Muntean_Iris_Lab2.Models;
+
+namespace Muntean_Iris_Lab2.Data
+{
+    public class Muntean_Iris_Lab2Context : DbContext
+    {
+        public Muntean_Iris_Lab2Context (DbContextOptions<Muntean_Iris_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Muntean_Iris_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
