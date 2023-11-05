7b51cad62e885292eca4f4bb8ae53492e65dc8fe
IRIS-GENȚIANA MUNTEAN <iris.muntean@stud.ubbcluj.ro>
2023-11-05 18:56:58 +0200

    lab4 eroare rezolvata

--- Pages/Books/Index.cshtml.cs
+++ Pages/Books/Index.cshtml.cs
@@ -46,8 +46,8 @@ namespace Muntean_Iris_Lab2.Pages.Books
             CurrentFilter = searchString;
 
             BookD.Books = await _context.Book
+                         .Include(b => b.Author)
                         .Include(b => b.Publisher)
-                        .Include(b => b.Author)
                         .Include(b => b.BookCategories)
                         .ThenInclude(b => b.Category)
                         .AsNoTracking()
