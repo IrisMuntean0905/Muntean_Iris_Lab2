42a38a5063d9d305a6e09f4f3fdf404dbb87da2b
IRIS-GENȚIANA MUNTEAN <iris.muntean@stud.ubbcluj.ro>
2023-11-16 18:20:49 +0200

    Laborator 6 rezolvat

--- Models/Book.cs
+++ Models/Book.cs
@@ -9,12 +9,16 @@ namespace Muntean_Iris_Lab2.Models
         public int ID { get; set; }
 
         [Display(Name = "Book Title")]
+
+        [Required(ErrorMessage = "Titlul cărții este obligatoriu.")]
+        [StringLength(150, MinimumLength = 3, ErrorMessage = "Titlul trebuie să aibă între 3 și 150 de caractere.")]
         public string Title { get; set; }
 
         public int? AuthorID { get; set; }
         public Author? Author { get; set; }
 
         [Column(TypeName = "decimal(6, 2)")]
+        [Range(0.01, 500)]
         public decimal Price { get; set; }
 
 
